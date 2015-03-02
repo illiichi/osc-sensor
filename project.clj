31de9c2e9951532a87316e33937cbd1939d21bd1
maeda <maeda@illi-ichi.com>
2015-03-02 22:43:27 +0900

    initial

---
+++ project.clj
@@ -0,0 +1,56 @@
+(defproject leindroid-sample/leindroid-sample "0.0.1-SNAPSHOT"
+  :description "FIXME: Android project description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :global-vars {*warn-on-reflection* true}
+
+  :source-paths ["src/clojure" "src"]
+  :java-source-paths ["src/java"]
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+  :plugins [[lein-droid "0.3.0-beta4"]]
+
+  :dependencies [[org.clojure-android/clojure "1.7.0-alpha3" :use-resources true]
+                 [overtone/osc-clj "0.9.0"]
+                 [overtone/at-at "1.2.0"]
+                 [neko/neko "3.1.0-preview2"]]
+  :profiles {:default [:dev]
+
+             :dev
+             [;; :android-common :android-user
+              {:dependencies [[org.clojure-android/tools.nrepl "0.2.6"]]
+               :target-path "target/debug"
+               :android {:aot :all-with-unused
+                         :rename-manifest-package "com.illiichi.debug"
+                         :manifest-options {:app-name "OSC-Sensor"}}}]
+             :release
+             [;; :android-common
+              {:target-path "target/release"
+               :android
+               { ;; Specify the path to your private keystore
+                ;; and the the alias of the key you want to
+                ;; sign APKs with.
+                ;; :keystore-path "/home/user/.android/private.keystore"
+                ;; :key-alias "mykeyalias"
+
+                :ignore-log-priority [:debug :verbose]
+                :aot :all
+                :build-type :release}}]}
+
+  :android {;; Specify the path to the Android SDK directory.
+            :sdk-path "/usr/local/Cellar/android-sdk/23.0.2/"
+
+            ;; Try increasing this value if dexer fails with
+            ;; OutOfMemoryException. Set the value according to your
+            ;; available RAM.
+            :dex-opts ["-JXmx4096M"]
+
+            ;; If previous option didn't work, uncomment this as well.
+            ;; :force-dex-optimize true
+
+            :target-version "15"
+            :aot-exclude-ns ["clojure.parallel" "clojure.core.reducers"
+                             "cljs-tooling.complete" "cljs-tooling.info"
+                             "cljs-tooling.util.analysis" "cljs-tooling.util.misc"
+                             "cider.nrepl" "cider-nrepl.plugin"]})
---
+++ src/clojure/com/illiichi/main.clj
@@ -0,0 +1,135 @@
+(ns com.illiichi.main
+  (:use [neko.activity :only [defactivity set-content-view! *a]]
+        [neko.find-view :only [find-view find-views]]
+        [neko.threading :only [on-ui]]
+        [overtone.at-at :only [every mk-pool stop-and-reset-pool!]]
+        [overtone.osc])
+    (:import (android.content Context)
+             (android.hardware SensorManager SensorEventListener Sensor SensorEvent)))
+
+(def state (atom {:start? false}))
+
+(def client (atom nil))
+(def listeners (atom nil))
+ 
+(def my-pool (mk-pool))
+(def acc-stack (agent []))
+(def rotate-stack (agent []))
+
+(defn send-every [id stack interval]
+  (let [mean #(/ (reduce + %) (count %))
+        task (fn [coll]
+               (when (not (empty? coll))
+                 (apply osc-send @client (str "/" id) (map mean coll)))
+               [])]
+    (every interval #(send-off stack task) my-pool)))
+  
+(defn push-vector [stack vals]
+  (let [push (fn [xs xss]
+               (if (empty? xss)
+                 (map vector xs)
+                 (map conj xss xs)))]
+    (send-off stack #(push vals %))))
+
+(defn err-handler-fn [ag ex]
+  (println "evil error occured: " ex " and we still have value " @ag))
+(set-error-handler! acc-stack err-handler-fn)
+(set-error-handler! rotate-stack err-handler-fn)
+
+(defn create-listeners [interval sensorManager]
+  (let [create (fn [id sensor-type stack]
+                 (let [listener (reify SensorEventListener
+                                  (onAccuracyChanged [this sensor value] ())
+                                  (onSensorChanged [this event]
+                                    (apply #'push-vector stack (seq (list (. event values))))))]
+                   (send-every id stack interval)
+                   (.. sensorManager
+                       (registerListener listener
+                                         (.getDefaultSensor sensorManager sensor-type)
+                                         ;; SensorManager/SENSOR_DELAY_FASTEST
+                                         ;; SensorManager/SENSOR_DELAY_UI
+                                         SensorManager/SENSOR_DELAY_GAME))
+                   listener))]
+    [(create "rotate" Sensor/TYPE_ROTATION_VECTOR rotate-stack)
+     (create "acc" Sensor/TYPE_LINEAR_ACCELERATION acc-stack)]))
+
+(defn start-send [address port interval sensorManager]
+  (for [stack [acc-stack rotate-stack]]
+    (send stack (fn [_] [])))
+  (reset! client (osc-client address port))
+  (reset! listeners (create-listeners interval sensorManager))
+  (prn "start"))
+
+(defn stop-send [sensorManager]
+  (map #(.unregisterListener sensorManager %) @listeners)
+  (stop-and-reset-pool! my-pool)
+  (reset! client nil)
+  (reset! listeners nil)
+  (prn "stop"))
+
+(defn toggle-state [activity]
+  (let [state (swap! state #(update-in % [:start?] not))
+        sensorManager (.getSystemService activity Context/SENSOR_SERVICE)]
+    (-> (find-views activity "button")
+        (first)
+        (.setText (if (:start? state)
+                    "停止する" "再開")))
+    (if (:start? state)
+      (let [address  (str      (.getText (find-view activity ::address)))
+            port     (Integer. (str (.getText (find-view activity ::port))))
+            interval (Integer. (str (.getText (find-view activity ::interval))))]
+        (start-send address port interval sensorManager))
+      (stop-send sensorManager))))
+
+(defactivity com.illiichi.MyActivity
+  :key :main
+  :on-create
+  (fn [this bundle]    
+    (on-ui
+      (set-content-view! (*a)
+        [:linear-layout {:orientation :vertical}
+         [:linear-layout {}
+          [:text-view {:text "address"}]
+          [:edit-text {:text "192.168.0.4" :id ::address}]]
+         [:linear-layout {}
+          [:text-view {:text "port"}]
+          [:edit-text {:text "1234" :id ::port}]]
+         [:linear-layout {}
+          [:text-view {:text "interval(ms)"}]
+          [:edit-text {:text "200" :id ::interval}]]
+         [:button {:id "button"
+                   :text "ready?" :on-click (fn [_] (toggle-state (*a)))}]]))))
+
+;; (
+;; SynthDef("dynenv", {arg val = 0, dur = 1, ch = 0;
+;; 	Out.ar(ch, DynEnv.ar(val, dur));
+;; }).add
+;; )
+;; (
+
+;; var controlGroup = Group.new;
+;; var soundGroup = Group.new;
+
+;; var rotateBus = Bus.audio(s, 1);
+;; var accBus = Bus.audio(s, 1);
+
+;; var acc = Synth("dynenv", [\ch, accBus], controlGroup);
+;; var rotate = Synth("dynenv", [\ch, rotateBus], controlGroup);
+;; acc.set(\dur, 1/(60 - 2));
+;; rotate.set(\dur, 1/(60 - 2));
+
+;; soundGroup.moveAfter(controlGroup);
+
+;; OSCFunc({|msg, time, addr, recvPort|
+;; 	var v;
+;; 	msg.removeAt(0);
+;; 	v = msg.collect({|x| x * x}).sum.sqrt;
+;; 	acc.set(\val, v);
+;; }, '/acc', nil,1234);
+
+;; OSCFunc({|msg, time, addr, recvPort|
+;; 	rotate.set(\val, msg.at(1));
+;; }, '/rotate', nil,1234);
+
+;; {PMOsc.ar(440 * In.ar(rotateBus).abs, 10 * In.ar(accBus).abs, 3)}.play(soundGroup)
+;; )
